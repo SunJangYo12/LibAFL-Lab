38bcafe87c977cc1c10d7ceacbd54aaaba7ff4d3
sunjangyo12 <sunjangyo12@gmail.com>
2025-06-16 16:47:49 +0700

    basic fuzzer

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,27 @@ extern crate libafl;
 extern crate libafl_bolts;
 
 use libafl::{
-    executors::ExitKind,
-    inputs::{BytesInput, HasTargetBytes},
+    executors::{ ExitKind, inprocess::InProcessExecutor },
+    inputs::{ BytesInput, HasTargetBytes },
+    state::StdState,
+    corpus::{ InMemoryCorpus, OnDiskCorpus },
+    monitors::SimpleMonitor,
+    events::SimpleEventManager,
+    schedulers::QueueScheduler,
+    fuzzer::StdFuzzer,
+    generators::RandPrintablesGenerator,
 };
-use libafl_bolts::AsSlice;
 
+use libafl_bolts::{
+    rands::StdRand,
+    AsSlice,
+    nonzero,
+};
 
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
 
+fn main()
+{
     let mut harness = |input: &BytesInput| {
         let target = input.target_bytes();
         let buf = target.as_slice();
@@ -25,11 +37,28 @@ fn main() {
         ExitKind::Ok
     };
 
-    // To test the panic:
-    let input = BytesInput::new(Vec::from("abc"));
-    #[cfg(feature = "panic")]
-    harness(&input);
+
+    let mut state = StdState::new(
+        StdRand::new(),
+        InMemoryCorpus::<BytesInput>::new(),
+        OnDiskCorpus::new(PathBuf::from("./crashes")).unwrap(),
+        &mut (),
+        &mut (),
+    )
+    .unwrap();
 
 
-    
+    let mon = SimpleMonitor::new(|s| println!("{s}"));
+    let mut mgr = SimpleEventManager::new(mon);
+
+
+    let scheduler = QueueScheduler::new();
+    let mut fuzzer = StdFuzzer::new(scheduler, (), ());
+
+    let mut executor = InProcessExecutor::new(&mut harness, (), &mut fuzzer, &mut state, &mut mgr).expect("Failed to create the Executor");
+
+
+    let mut generator = RandPrintablesGenerator::new(nonzero!(32));
+
+    state.generate_initial_inputs(&mut fuzzer, &mut executor, &mut generator, &mut mgr, 8).expect("Failed to generate the initial corpus");
 }
